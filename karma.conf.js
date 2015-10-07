325d8b84669882ff53f4cbadbcbacfc1abb0c801
Gustavo Cardoso <gustavocardoso@gmail.com>
2015-10-07 12:59:07 -0300

    Changing some configuration on karma config file.

--- karma.conf.js
+++ karma.conf.js
@@ -56,7 +56,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['Firefox'],
 
 
     // Continuous Integration mode
@@ -65,7 +65,7 @@ module.exports = function(config) {
 
     plugins: [
       'karma-jasmine',
-      'karma-chrome-launcher',
+      'karma-firefox-launcher',
       //'karma-firefox-launcher',
       //'karma-safari-launcher'
     ]
